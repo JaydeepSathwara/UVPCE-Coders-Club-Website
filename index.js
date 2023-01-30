e6292990485363e379833bded21bf834dd59e4e6
jaydeep <jaydeepsathwara272@gmail.com>
2023-01-30 23:40:08 +0530

    Hero Section Done

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ $(document).ready(function(){
     $('#menu').click(function(){
         $(this).toggleClass('fa-times')
         $('.navbar').toggleClass('nav-toggle')
-    })
+    });
+
+    $(window).on('load scroll', function(){
+        $("#menu").toggleClass('fa-times')
+        $('.navbar').toggleClass('nav-toggle')
+    });
 })
